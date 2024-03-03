4902f75b2fd452255f8fb5d105c8e4b59a8ad350
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-03-03 14:58:06 +0900

    Joi init

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,8 +4,30 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@boilerplate/libs';
 
 import { UsersControllerModule } from './users/users.controller.module';
+import Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule, PrismaModule.forPostgre(), UsersControllerModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number().port().default(3000),
+        USE_DOC: Joi.boolean().default(true),
+        POSTGRE_DATABASE_URL: Joi.string()
+          .custom((value, helpers) => {
+            if (!value) {
+              return helpers.error('postgresql is necessary');
+            }
+            const reg = /^(postgresql)/;
+            if (!reg.test(value)) {
+              return helpers.error('postgresql Host 형식이 맞지 않음');
+            }
+            return value;
+          })
+          .required(),
+      }),
+    }),
+    PrismaModule.forPostgre(),
+    UsersControllerModule,
+  ],
 })
 export class AppModule {}
