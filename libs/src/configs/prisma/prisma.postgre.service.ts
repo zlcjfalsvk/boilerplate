23fab34124ebb469f3a54ee27e15b8fa19cdc045
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-04-01 14:56:55 +0900

    prisma 관련 수정

--- libs/src/configs/prisma/prisma.postgre.service.ts
+++ libs/src/configs/prisma/prisma.postgre.service.ts
@@ -1,20 +1,50 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-
 import { PrismaClient } from '@prisma/postgre';
+import * as process from 'process';
+
+import { PrismaClientOptions } from '@prisma/postgre/runtime/library';
 
 @Injectable()
 export class PrismaPostgreService
-  extends PrismaClient
+  extends PrismaClient<PrismaClientOptions, 'info' | 'warn' | 'query' | 'error'>
   implements OnModuleInit, OnModuleDestroy
 {
   constructor(host: string) {
+    const isDevEnv = process.env['ENABLE_PRISMA_LOG'] || false;
     super({
       datasources: {
         db: {
           url: host,
         },
       },
+      log: isDevEnv
+        ? [
+            {
+              level: 'query',
+              emit: 'event',
+            },
+            { level: 'info', emit: 'stdout' },
+            { level: 'warn', emit: 'stdout' },
+            { level: 'error', emit: 'stdout' },
+          ]
+        : [],
     });
+    if (isDevEnv) {
+      this.$on('query', (e) => {
+        // console.log(e);
+        let queryWithParams = e.query;
+        const params = JSON.parse(e.params);
+        params.forEach((param: any, index: number) => {
+          queryWithParams = queryWithParams.replace(
+            `$${index + 1}`,
+            `'${param}'`,
+          );
+        });
+
+        console.info(`${e.timestamp}`);
+        console.info(`Query: ${queryWithParams}`);
+      });
+    }
   }
   async onModuleInit() {
     await this.$connect();
