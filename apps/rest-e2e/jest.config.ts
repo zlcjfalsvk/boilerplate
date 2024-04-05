a4b21fab67814b4ce2e6702be8e8ca06e5ad746e
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-04-05 10:55:43 +0900

    move project api -> rest

--- apps/rest-e2e/jest.config.ts
+++ apps/rest-e2e/jest.config.ts
@@ -1,15 +1,18 @@
 /* eslint-disable */
 export default {
-  displayName: 'api-e2e',
+  displayName: 'rest-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
   setupFiles: ['<rootDir>/src/support/test-setup.ts'],
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    }],
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/api-e2e',
