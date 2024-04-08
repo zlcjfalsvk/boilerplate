39e773f1282b22ea93b1765a9d77f7b631152134
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-04-08 10:53:07 +0900

    generate libs/service

---
+++ libs/service/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'service',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/service',
+  moduleNameMapper: {
+    '^@prisma/postgre(|/.*)$': '<rootDir>/../../prisma/postgre/client/$1',
+  },
+};
