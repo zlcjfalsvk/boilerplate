352fc0ae1e5b7fad7ab4fee79be1f21c131c1058
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-04-24 21:11:56 +0900

    jest code

--- apps/rest-e2e/src/support/global-setup.ts
+++ apps/rest-e2e/src/support/global-setup.ts
@@ -1,9 +1,19 @@
 /* eslint-disable */
+import * as child_process from 'node:child_process';
+import * as process from 'node:process';
+
 var __TEARDOWN_MESSAGE__: string;
 
 module.exports = async function () {
   // Start services that that the app needs to run (e.g. database, docker-compose, etc.).
   console.log('\nSetting up...\n');
+
+  child_process.execSync('docker-compose up -d', { stdio: 'inherit' });
+  console.log('\nCompleted docker-compose up...\n');
+
+  process.env.POSTGRE_DATABASE_URL =
+    'postgresql://postgres:cheolcheol@localhost:5432/boilerplate?schema=public';
+  process.env.USE_DOC = 'false';
 
   // Hint: Use `globalThis` to pass variables to global teardown.
   globalThis.__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
--- apps/rest-e2e/src/support/global-teardown.ts
+++ apps/rest-e2e/src/support/global-teardown.ts
@@ -1,7 +1,14 @@
 /* eslint-disable */
 
+import * as child_process from 'node:child_process';
+
 module.exports = async function () {
   // Put clean up logic here (e.g. stopping services, docker-compose, etc.).
   // Hint: `globalThis` is shared between setup and teardown.
   console.log(globalThis.__TEARDOWN_MESSAGE__);
+
+  // docker-compose
+  console.log('\ndocker-compose down...\n');
+  child_process.exec('docker-compose down -v');
+  console.log('\ndocker-compose down completed...\n');
 };
