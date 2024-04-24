10379d41fd5e9e6aabafede374ce2937efd67f41
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-04-24 21:13:07 +0900

    rename apps: rest -> http

--- apps/http-e2e/jest.config.ts
+++ apps/http-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'rest-e2e',
+  displayName: 'http-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
